a09cffebc5918a09b1c3b12d46f22fba6d378636
davout <david.francois@webflows.fr>
2014-10-13 14:07:42 +0200

    WIP

--- spec/fix/engine_spec.rb
+++ spec/fix/engine_spec.rb
@@ -4,14 +4,14 @@ describe 'Fix::Engine' do
 
   describe '.alias_namespace!' do
     it 'should have already been called' do
-      FE.should be(Fix::Engine)
+      expect(FE).to be(Fix::Engine)
     end
   end
 
   describe '.run!' do
     it 'should create a FE::Server instance and start it' do
-      FE::Server.should_receive(:new).with('foo', 'bar').once.and_call_original
-      FE::Server.any_instance.should_receive(:run!).once
+      expect(FE::Server).to receive(:new).with('foo', 'bar').once.and_call_original
+      expect(FE::Server.any_instance).to receive(:run!).once
       FE.run!('foo', 'bar')
     end
   end
